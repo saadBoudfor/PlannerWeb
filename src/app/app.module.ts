5a2565c400e1a02aee8945997b3bc6841c5b73af
saad boudfor <saad.boudfor@capgemini.com>
2017-12-28 19:14:31 +0100

    Init project: add animation library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import { MzButtonModule } from 'ng2-materialize';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    MzButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
